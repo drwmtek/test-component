773d7516f2147d7504ebae26f447ba6fc1550b76
drwmtek <iron45pc45@gmail.com>
2017-10-03 11:17:20 +0500

    code comment

--- local/components/olof/slider/class.php
+++ local/components/olof/slider/class.php
@@ -1,18 +1,18 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 /**
- USING
- <?$APPLICATION->IncludeComponent(
-	"olof:slider", 
-	"bootstrap", 
-	array(
-		"COMPONENT_TEMPLATE" => "bootstrap",
-		"ITEMS" => Array(
-			Array("ID" => 1, "ACTIVE" => false, "PROPERTY_IMAGE" => "image_url.png", "NAME" => "firstName"),
-			Array("ID" => 2, "ACTIVE" => true, "PROPERTY_IMAGE" => "image_url.jpg", "NAME" => "secondName")
-		)
-	),
-	false
-);?>
+ * USING
+ * <?$APPLICATION->IncludeComponent(
+ *	"olof:slider", 
+ *	"bootstrap", 
+ *	array(
+ *		"COMPONENT_TEMPLATE" => "bootstrap",
+ *		"ITEMS" => Array(
+ *			Array("ID" => 1, "ACTIVE" => false, "PROPERTY_IMAGE" => "image_url.png", "NAME" => "firstName"),
+ *			Array("ID" => 2, "ACTIVE" => true, "PROPERTY_IMAGE" => "image_url.jpg", "NAME" => "secondName")
+ *		)
+ *	),
+ *	false
+ * );?>
 */
 class COlofSlider extends CBitrixComponent
 {
